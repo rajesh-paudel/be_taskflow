c3d4a191e83339981cb6031cb5249ab83328d211
Rajesh Paudel <rajeshpaudel9863@gmail.com>
2026-06-02 11:56:03 +0545

    changes

--- src/server.js
+++ src/server.js
@@ -1,19 +1,29 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.routes.js";
+import { protect } from "./middleware/auth.middleware.js";
 dotenv.config();
-
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+connectDB();
+
+app.use("/api/auth", authRoutes);
+
+app.get("/api/protected", protect, (req, res) => {
+  res.json({
+    message: "You accessed protected data 🔐",
+    user: req.user,
+  });
+});
 app.get("/", (req, res) => {
   res.send("hello");
 });
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
